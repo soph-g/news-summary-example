8be4569cc16014491d3743fdc0363ec686e8c3d1
Sophie Gill <sophie@makersacademy.com>
2019-07-16 12:33:13 +0100

    articles are now fetched and logged to the console

--- src/articleController.js
+++ src/articleController.js
@@ -1,5 +1,9 @@
-function ArticleController() {
-
+function ArticleController(articleModel, articleView) {
+  this.model = articleModel;
+  this.view = articleView;
 }
 
-ArticleController.prototype.updateDOM = function() {}
+ArticleController.prototype.updateDOM = function() {
+  // ask Model to get data
+  this.model.getHeadlines()
+}
--- src/articleModel.js
+++ src/articleModel.js
@@ -1,3 +1,18 @@
 function ArticleModel() {
-  
+
+}
+
+ArticleModel.prototype.getHeadlines = function() {
+  fetch('http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?from-date=2019-07-01')
+    .then(res => res.json())
+    .then(data => {
+      return data.response.results.map((article) => {
+        return {
+          title: article.webTitle,
+          id: article.id,
+          url: article.apiUrl
+        }
+      });
+    })
+    .then(arr => console.log(arr))
 }
